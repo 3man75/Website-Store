36b47cd299c6c07eb57fcf42f7e361b2d3079227
3man75 <JavierM6456@gmail.com>
2022-04-16 18:49:53 -0400

    first

---
+++ package/market.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+db.create_all()
+
+#local database that will hold items
+class Item(db.Model):
+    #each of these lines will be elements in our db table
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    price = db.Column(db.Integer(), nullable=False)
+    barcode = db.Column(db.String(length=12), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+
+
+    def __repr__(self):
+        return f'Item {self.name}'
+
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():  #market page with items
+
+      items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+      ]
+
+      
+      return render_template('market.html', items=items)
+
+if __name__== "__main__":
+     app.run(debug = True)
+
+
+
+     #xml
+     #javascript / bootstrap
